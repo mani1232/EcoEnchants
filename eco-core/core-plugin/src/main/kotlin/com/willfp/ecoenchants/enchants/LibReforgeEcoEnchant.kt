db96f28ca45402a33665f15036339b91cda916ff
mani1232 <perederkoura@gmail.com>
2023-11-28 23:34:06 +0100

    Fix 1.20

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoenchants/enchants/LibReforgeEcoEnchant.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoenchants/enchants/LibReforgeEcoEnchant.kt
@@ -5,7 +5,6 @@ import com.willfp.ecoenchants.EcoEnchantsPlugin
 import com.willfp.libreforge.SilentViolationContext
 import com.willfp.libreforge.ViolationContext
 import com.willfp.libreforge.effects.Effects
-import com.willfp.libreforge.effects.emptyEffectList
 
 class LibReforgeEcoEnchant(
     id: String,
